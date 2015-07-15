b7b52305e8561b7bdf830d19b94e6e72dd1b2bb7
GrafRaf <grafraf@mail.ru>
2015-07-15 09:57:35 +0600

    -

--- SPLogBeagle/SPLogBeagle.Web/Scripts/init.js
+++ SPLogBeagle/SPLogBeagle.Web/Scripts/init.js
@@ -54,7 +54,7 @@
             $scope.error = "";
             $http({
                 method: 'GET',
-                url: "/scripts/locations-test.json",
+                url: "/scripts/locations-test.json",  
                 headers: { 'Content-Type': 'application/json' }
             })
             .success(function (data, status, headers, config) {
